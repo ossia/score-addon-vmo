fc1d3e66172df65f200dfe8adbd6a52030f865e2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-09-24 11:40:28 +0200

    use python vmo

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Provide the VMO audio improvisation algorithm
 
+
+# Patches & fixes required:
+
+- https://stackoverflow.com/questions/29880931/importerror-and-pyexc-systemerror-while-embedding-python-script-within-c-for-pam
+- https://stackoverflow.com/questions/48423782/importerror-cannot-import-name-distributor-init
--- vmo/Executor.cpp
+++ vmo/Executor.cpp
@@ -5,18 +5,82 @@
 #include <ossia/dataflow/port.hpp>
 
 #include <vmo/Process.hpp>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wregister"
+#include <pybind11/embed.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <dlfcn.h>
+#pragma GCC diagnostic pop
+namespace py = pybind11;
 namespace vmo
 {
+void vmo_context()
+{
+}
+
 class node final : public ossia::nonowning_graph_node
 {
+  ossia::inlet input{ossia::audio_port{}};
+  ossia::inlet active{ossia::value_port{}};
+  ossia::outlet output{ossia::audio_port{}};
+
+  bool m_is_active{};
+
+  std::vector<std::vector<float>> data;
 public:
   node()
   {
+    vmo_context();
   }
 
   void
   run(ossia::token_request tk, ossia::exec_state_facade s) noexcept override
   {
+    try
+    {
+      auto& i = *input.data.target<ossia::audio_port>();
+      auto& ok = *active.data.target<ossia::value_port>();
+      auto& o = *output.data.target<ossia::audio_port>();
+
+      for(const auto& v : ok.get_data())
+      {
+        m_is_active = ossia::convert<bool>(v.value);
+      }
+
+      if(m_is_active)
+      {
+
+      }
+      else
+      {
+        static bool ok = false;
+        if(!ok)
+        {
+          ok = true;
+          using namespace py::literals;
+
+          dlopen("libpython2.7.so", RTLD_LAZY |  RTLD_GLOBAL);
+          py::scoped_interpreter guard{};
+          py::module oracle{py::module::import("vmo.VMO.oracle")};
+
+          std::vector<std::string> seq = {"a", "b" , "b", "c", "a", "b", "c", "d", "a", "b", "c" };
+
+          auto p = oracle.attr("build_oracle")(seq, "f");
+          auto locals = py::dict("p"_a=p);
+          py::exec(R"_(
+                   print(str([0, 0, 0, 1, 0, 1, 2, 2, 0, 1, 2, 3]) + " == " + str(p.lrs))
+                   print(str([None, 0, 0, 2, 0, 1, 2, 4, 0, 1, 2, 7]) + " == " + str(p.sfx))
+                   )_", py::globals(), locals);
+
+        }
+
+      }
+    }
+    catch(std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+    }
   }
 
   std::string label() const noexcept override
@@ -32,9 +96,16 @@ ProcessExecutorComponent::ProcessExecutorComponent(
     const Id<score::Component>& id, QObject* parent)
     : ProcessComponent_T{element, ctx, id, "vmoExecutorComponent", parent}
 {
-  auto n = std::make_shared<vmo::node>();
-  this->node = n;
-  m_ossia_process = std::make_shared<ossia::node_process>(n);
+  try
+  {
+    auto n = std::make_shared<vmo::node>();
+    this->node = n;
+    m_ossia_process = std::make_shared<ossia::node_process>(n);
+  }
+  catch(std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
 
   /** Don't forget that the node executes in another thread.
    * -> handle live updates with the in_exec function, e.g. :
